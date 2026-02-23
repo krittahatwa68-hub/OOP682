4bd8115a157dee7064803a9d56db160a563aae74
krittahatwa68-hub <krittahat.wa.68@ubu.ac.th>
2026-02-23 09:38:35 +0700

    Challege

---
+++ log_viewer/main.py
@@ -0,0 +1,13 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from ui.main_window import MainWindow
+from services.factory import SourceFactory
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    source = SourceFactory.create_source("csv") 
+    window = MainWindow(source)
+    window.resize(400, 300)
+    window.show()
+
+    sys.exit(app.exec())
